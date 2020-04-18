5c2fa32c0859ce7dbf43fd0bccb467e01e12b69d
RafaYano <31872365+RafaYano@users.noreply.github.com>
2020-04-18 14:56:51 -0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,14 @@
+import Pyro4
+
+@Pyro4.expose
+class Hello(object):
+    def say_hello(self):
+        return 'Hello World'
+        
+daemon = Pyro4.Daemon()             #cria um daemon do Pyro
+ns = Pyro4.locateNS()               #procura o servidor DNS
+uri = daemon.register(Hello)        #registra Hello no Pyro
+ns.register("example.hello", uri)   #registra Hello no servidor DNS
+
+print("Servidor Pronto.")
+daemon.requestLoop()                #inicia a espera por chamadas
